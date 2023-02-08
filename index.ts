4da09051225bce4522936ef901494eb766ce3607
Frederik Lölhöffel <6864947+floelhoeffel@users.noreply.github.com>
2023-02-08 12:32:43 +0100

    Added a loop so npm run dev hits accel n times

--- index.ts
+++ index.ts
@@ -2,30 +2,47 @@ import { PrismaClient } from "@prisma/client";
 import useAccelerate from "@prisma/extension-accelerate";
 
 const prisma = new PrismaClient().$extends(useAccelerate);
+let iteration = 0;
+let totalTime = 0;
 
-async function main() {
-  const startTime = Date.now();
-
+async function createData() {
   // await prisma.user.create({
   //   data: {
   //     email: "loelhoeffel3@prisma.io",
   //   },
   // });
-
   // await prisma.user.create({
   //   data: {
   //     email: "loelhoeffel4@prisma.io",
   //   },
   // });
+}
+
+async function getData() {
+  if (iteration > 100) {
+    console.log("Total time", totalTime);
+    console.log("Average request time", totalTime / iteration);
+    return;
+  }
+  const startTime = Date.now();
 
   const { data, info } = await prisma.user
     .count({
-      cacheStrategy: { swr: 60, ttl: 60 },
+      cacheStrategy: { ttl: 3600},
     })
     .withAccelerateInfo();
 
+  console.log("Iteration", iteration);
   console.dir(info);
-  console.log("Request took:", Date.now() - startTime, "ms");
+  const time = Date.now() - startTime;
+  totalTime = totalTime + time;
+  console.log("Request took:", time, "ms");
+  iteration++;
+  getData();
+}
+
+async function main() {
+  getData();
 }
 
 main()
